acf568ccdb62b2f97fbfb11c9e579b6345d74d99
Alexander Heim <47790717+AlexanderHeim@users.noreply.github.com>
2019-10-10 12:30:50 +0200

    Update Pokerbeispiel.java

--- io/github/alexander/pokerbeispiel/Pokerbeispiel.java
+++ io/github/alexander/pokerbeispiel/Pokerbeispiel.java
@@ -1,9 +1,24 @@
 package io.github.alexander.pokerbeispiel;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
 public class Pokerbeispiel {
 
 	public static void main(String[] args) {
 		ProbabilityCounter pc = new ProbabilityCounter(new Carddeck());
-		pc.printProbabilities(10000000);
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        	System.out.print("How many shuffles:");
+        	try {
+            		int i = Integer.parseInt(br.readLine());
+			pc.printProbabilities(i);
+        	} catch(NumberFormatException nfe) {
+            		System.err.println("Invalid Format!");
+        	}
+		
+		
 	}
 }
