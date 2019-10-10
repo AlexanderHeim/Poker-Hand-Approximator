7735d31f6a7d444fbb7d9f9745f2fcc67ebef2c1
Alexander Heim <47790717+AlexanderHeim@users.noreply.github.com>
2019-10-10 12:28:33 +0200

    Update Pokerbeispiel.java

--- io/github/alexander/pokerbeispiel/Pokerbeispiel.java
+++ io/github/alexander/pokerbeispiel/Pokerbeispiel.java
@@ -4,8 +4,6 @@ public class Pokerbeispiel {
 
 	public static void main(String[] args) {
 		ProbabilityCounter pc = new ProbabilityCounter(new Carddeck());
-		System.out.println("How many shuffles?");
-		String input = System.console().readLine();
 		pc.printProbabilities(10000000);
 	}
 }
